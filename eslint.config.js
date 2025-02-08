90fcf6aecff73b7d8ee401d49530a559a46dcb6a
Gaby Carbajal <gabyalvarez159@gmail.com>
2025-02-07 21:27:22 -0500

    formater config

---
+++ eslint.config.js
@@ -0,0 +1,78 @@
+// @ts-check
+const prettierPlugin = require('eslint-plugin-prettier');
+const typescriptParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const angularPlugin = require('@angular-eslint/eslint-plugin');
+const angularTemplateParser = require('@angular-eslint/template-parser');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+module.exports = [
+  {
+    ignores: ['.cache/', '.git/', '.github/', 'node_modules/'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: [
+          './tsconfig.json',
+          './tsconfig.app.json',
+          './tsconfig.spec.json',
+        ],
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      '@angular-eslint': angularPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...angularPlugin.configs.recommended.rules,
+      ...prettierPlugin.configs?.rules,
+      '@angular-eslint/directive-selector': [
+        'warn',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'warn',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      '@typescript-eslint/member-ordering': 'error',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'variable',
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+          trailingUnderscore: 'forbid',
+        },
+      ],
+      '@angular-eslint/no-host-metadata-property': 'off',
+      '@angular-eslint/no-output-on-prefix': 'off',
+      '@typescript-eslint/ban-types': 'off',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    languageOptions: {
+      parser: angularTemplateParser,
+    },
+    plugins: {
+      '@angular-eslint': angularPlugin,
+      '@angular-eslint/template': angularPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': ['error', { parser: 'angular' }],
+    },
+  },
+  eslintPluginPrettierRecommended,
+];
