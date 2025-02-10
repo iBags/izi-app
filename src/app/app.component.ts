24a095e6aa627a30988bcdf2a8e15f281195f348
Gaby Carbajal <gabyalvarez159@gmail.com>
2025-02-09 22:27:20 -0500

    cleaning app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,10 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from './ui/button/button.component';
-import { ButtonGroupComponent } from './ui/button-group/button-group.component';
 import { LayoutComponent } from './components/layout/layout.component';
-import { DaySelectorComponent } from './ui/calendar/day-selector/day-selector.component';
-import { ContainerComponent } from './components/container/container.component';
-import { StepperComponent } from './ui/stepper/stepper.component';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'izi-root',
-  imports: [
-    ButtonComponent,
-    ButtonGroupComponent,
-    LayoutComponent,
-    DaySelectorComponent,
-    ContainerComponent,
-    StepperComponent,
-    RouterLink,
-    RouterLinkActive,
-    RouterOutlet,
-  ],
+  imports: [LayoutComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
